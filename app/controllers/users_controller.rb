2080a19f4f1117e266b35196222ebae4a98f4d01
aocattleya <aocattleya@yahoo.co.jp>
2019-10-24 20:11:22 +0900

    update: ツイート一覧画面にニックネームを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @nickname = current_user.nickname
-    @tweets = current_user.tweets
+    user = User.find(params[:id])
+    @nickname = user.nickname
+    @tweets = user.tweets
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_24_103457) do
+ActiveRecord::Schema.define(version: 2019_10_24_110531) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
