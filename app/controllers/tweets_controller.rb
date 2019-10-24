73eb36afe6d1b74a5bff8f4a692f9b0cbaff0a02
aocattleya <aocattleya@yahoo.co.jp>
2019-10-24 16:49:00 +0900

    add: ツイート詳細表示機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,6 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+
   def index
     @tweets = Tweet.all
   end
@@ -17,12 +19,14 @@ class TweetsController < ApplicationController
   end
 
   def edit
-    @tweet = Tweet.find(params[:id])
   end
 
   def update
     tweet = Tweet.find(params[:id])
     tweet.update(tweet_params)
+  end
+
+  def show
   end
 
   private
