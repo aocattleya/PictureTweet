36f4a387b69e32557e2ce6470b411ac94af42a3c
aocattleya <aocattleya@yahoo.co.jp>
2019-10-24 20:44:36 +0900

    add: ページネーションの実装 & 部分テンプレートの利用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @tweets = user.tweets
+    @tweets = user.tweets.page(params[:page]).per(5).order("created_at DESC")
   end
 end
