78f77d99350818b1d5a2a45ab602cff620e4c6a1
aocattleya <aocattleya@yahoo.co.jp>
2019-11-13 12:20:59 +0900

    add: コメント機能に非同期通信を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,10 @@
 class CommentsController < ApplicationController
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/tweets/#{comment.tweet.id}"
+    @comment = Comment.create(text: comment_params[:text], tweet_id: comment_params[:tweet_id], user_id: current_user.id)
+    respond_to do |format|
+      format.html { redirect_to tweet_path(params[:tweet_id])  }
+      format.json
+    end
   end
 
   private
